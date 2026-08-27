6f67d5910ea0b74b04d6113803a9cc02f51049e4
TaprootFreakAI <315477232+TaprootFreakAI@users.noreply.github.com>
2026-08-27 10:35:57 +0200

    style: sort fake_async import before flutter_test

--- test/integration/sell_bitbox_flow_test.dart
+++ test/integration/sell_bitbox_flow_test.dart
@@ -23,8 +23,8 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:fake_async/fake_async.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/testing.dart';
 import 'package:mocktail/mocktail.dart';
